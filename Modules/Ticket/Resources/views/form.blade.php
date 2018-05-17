968005f204e975daaa2b8482da6adeb2cfa99a70
UzzairBaharudin <uze@brillanteinsights.com>
2018-05-17 11:19:24 +0800

    changed remarks to comments

--- Modules/Ticket/Resources/views/form.blade.php
+++ Modules/Ticket/Resources/views/form.blade.php
@@ -174,11 +174,11 @@
             <div class="card"> 
                     @cardHeader 
                     @slot('card_title')
-                    <i class="fe fe-message-circle"></i> Remarks @endslot 
+                    <i class="fe fe-message-circle"></i> Comments @endslot 
                     @endcardHeader 
                     @cardBody   
                     <div class="form-group" @if ($errors->has('replybody')) has-error @endif>
-                        <label for="" class="form-label">Leave a remark</label>
+                        <label for="" class="form-label">Leave a comment</label>
                     <textarea name="replybody" id="" cols="30" rows="5" class="form-control"></textarea>
                         @if ($errors->has('replybody'))
                         <p class="text-danger">{{ $errors->first('replybody') }}</p>
